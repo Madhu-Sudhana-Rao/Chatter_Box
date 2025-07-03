540416635452dc2fde356940fe7b3701a067a47d
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-03 08:58:10 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,13 +6,18 @@ import chatRoutes from "./routes/chat_routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
 }));
 
@@ -22,6 +27,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 connectDB().then(() => {
     const PORT = process.env.PORT || 5001;
