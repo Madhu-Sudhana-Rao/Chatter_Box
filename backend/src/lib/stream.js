a9da1b1aa9d474ce34dfdd8fe6300a9dce9a8ba6
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-03 14:55:52 +0530

    Update stream.js

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,39 +1,46 @@
 import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
+let streamClient = null;
+
 const getStreamClient = () => {
-    const apiKey = process.env.STEAM_API_KEY;
-    const apiSecret = process.env.STEAM_API_SECRET;
+  if (streamClient) return streamClient;
 
-    if (!apiKey || !apiSecret) {
-        console.error("Stream API key or Secret is missing");
-        throw new Error("Missing Stream credentials");
-    }
+  const apiKey = process.env.STEAM_API_KEY;
+  const apiSecret = process.env.STEAM_API_SECRET;
 
-    return StreamChat.getInstance(apiKey, apiSecret);
+  if (!apiKey || !apiSecret) {
+    console.error("Stream API key or Secret is missing");
+    throw new Error("Missing Stream credentials");
+  }
+
+  streamClient = StreamChat.getInstance(apiKey, apiSecret);
+  return streamClient;
 };
+
 
 export const upsertStreamUser = async (userData) => {
-    try {
-        const client = getStreamClient();
-        await client.upsertUsers([userData]);
-        return userData;
-    } catch (error) {
-        console.error("Error creating stream user:", error.message);
-        throw error;
-    }
+  try {
+    const client = getStreamClient();
+    await client.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.error("Error creating stream user:", error.message);
+    throw error;
+  }
 };
 
-export const generateStreamToken = (userId) => {
-    try {
-        if (!userId) {
-            throw new Error("Missing userId for token generation");
-        }
 
-        const client = getStreamClient();
-        return client.createToken(userId.toString());
-    } catch (error) {
-        console.error("Error generating Stream token:", error.message);
-        return null;
+export const generateStreamToken = (userId) => {
+  try {
+    if (!userId) {
+      throw new Error("Missing userId for token generation");
     }
+
+    const client = getStreamClient();
+    return client.createToken(userId.toString());
+  } catch (error) {
+    console.error("Error generating Stream token:", error.message);
+    return null;
+  }
 };
