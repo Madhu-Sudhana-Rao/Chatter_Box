f6150841bafe56b2bfed458aca3ba4586c8be98d
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-03 13:27:50 +0530

    Update chat_controllers.js

--- backend/src/controllers/chat_controllers.js
+++ backend/src/controllers/chat_controllers.js
@@ -1,12 +1,16 @@
 import { generateStreamToken } from "../lib/stream.js";
 
-export async function getStreamToken(req, res) {
+export const getChatToken = async (req, res) => {
     try {
-        const token = generateStreamToken(req.user.id);
+        const userId = req.userId;
+        if (!userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
 
-        res.status(200).json({ token });
+        const token = generateStreamToken(userId);
+        return res.status(200).json({ token });
     } catch (error) {
-        console.log("Error in getStreamToken controller:", error.message);
-        res.status(500).json({ message: "Internal Serer Error" });
+        console.error("Token Error:", error.message);
+        res.status(500).json({ message: "Token generation failed" });
     }
-}
+};
