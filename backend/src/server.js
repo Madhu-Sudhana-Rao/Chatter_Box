20b13fa2e753325866fe32affb3c52359e00410c
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-03 10:09:15 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.get("*", (req, res) => {
 });
 
 connectDB().then(() => {
-    const PORT = process.env.PORT || 5001;
+    const PORT = process.env.PORT;
     app.listen(PORT, () => {
         console.log(`Server running at http://localhost:${PORT}`);
     });
