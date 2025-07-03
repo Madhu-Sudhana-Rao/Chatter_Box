2aa4746a55cf57ee5805bea6786941b6ffd174ae
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-03 12:01:50 +0530

    Update useAuthUser.js

--- frontend/chat/src/hooks/useAuthUser.js
+++ frontend/chat/src/hooks/useAuthUser.js
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getAuthUser } from '../lib/api';
 
 const useAuthUser = () => {
-    const { data, isLoading } = useQuery({
-        queryKey: ['authUser'],
-        queryFn: getAuthUser,
-        retry: false,
-    });
+  const { data, isLoading } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: getAuthUser,
+    retry: false,
+  });
 
-    const authUser = data?.user || data || null;
+  const authUser = data?.user || data || null;
 
-    return { isLoading, authUser };
+  return { isLoading, authUser };
 };
 
 export default useAuthUser;
