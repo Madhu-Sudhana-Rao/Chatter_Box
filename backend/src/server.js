a78dc8b900ac0af57d887d0bb9653b9fa06ffa0a
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-03 11:43:22 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,20 +6,17 @@ import chatRoutes from "./routes/chat_routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(cors({
-    origin: true,
+app.use(
+  cors({
+    origin: "https://chatter-box-3.onrender.com",
     credentials: true,
-}));
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,15 +25,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
-
 connectDB().then(() => {
-    const PORT = process.env.PORT;
-    app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
-    });
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
 });
