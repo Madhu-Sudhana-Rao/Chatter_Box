dfdabded346d2a1ae0faab03815a6f08b9aab375
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-21 22:35:57 +0530

    Update stream.js

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,11 +1,11 @@
 import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
-const apiKey = process.env.STREAM_API_KEY;
-const apiSecret = process.env.STREAM_API_SECRET;
+const apiKey = process.env.STEAM_API_KEY;
+const apiSecret = process.env.STEAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-  console.error("Missing STREAM_API_KEY or STREAM_API_SECRET");
+  console.error("Missing STEAM_API_KEY or STEAM_API_SECRET");
   throw new Error("Missing Stream credentials");
 }
 
