372ed79bb7b52051d612fefacf5099b91346f014
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-23 21:39:47 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,28 @@
-import mongoose from "mongoose";
+import { generateStreamToken, upsertStreamUser } from "../lib/stream.js";
 
-export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected:${conn.connection.host}`)
-    } catch (error) {
-        console.log("Error in connecting to MongoDB",error)
-        process.exit(1);
+export async function getStreamToken(req, res) {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized: User ID missing" });
     }
+
+    await upsertStreamUser({
+      id: userId,
+      name: req.user?.fullName || "Anonymous",
+      image: req.user?.profilePic || "",
+    });
+
+    const token = generateStreamToken(userId);
+
+    if (!token) {
+      return res.status(500).json({ message: "Token generation failed" });
+    }
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error("Error in getStreamToken controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
