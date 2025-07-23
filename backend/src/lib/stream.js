4d9bea8e3081798740444ab398361203d4732813
Madhu-Sudhana-Rao <154989674+Madhu-Sudhana-Rao@users.noreply.github.com>
2025-07-23 21:34:01 +0530

    Update stream.js

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,3 +1,4 @@
+// stream.js
 import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
@@ -5,7 +6,7 @@ const apiKey = process.env.STEAM_API_KEY;
 const apiSecret = process.env.STEAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-  console.error("Stream API key or Secret is missing");
+  console.error("STEAM API key or Secret is missing");
 }
 
 const streamClient = StreamChat.getInstance(apiKey, apiSecret);
@@ -21,10 +22,10 @@ export const upsertStreamUser = async (userData) => {
 
 export const generateStreamToken = (userId) => {
   try {
-    // ensure userId is a string
     const userIdStr = userId.toString();
     return streamClient.createToken(userIdStr);
   } catch (error) {
     console.error("Error generating Stream token:", error);
+    return null;
   }
 };
